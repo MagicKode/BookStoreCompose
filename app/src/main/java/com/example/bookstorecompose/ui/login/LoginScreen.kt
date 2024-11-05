23104fcc5acae60994854990aec0d0edbfa2b664
dmi6756@yandex.ru <a0zvx753>
2024-11-05 15:20:03 +0300

    1. Создание возможности Входа/Выхода/удаления аккаунта

--- app/src/main/java/com/example/bookstorecompose/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/bookstorecompose/ui/login/LoginScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.google.firebase.Firebase
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 
@@ -29,7 +30,7 @@ fun LoginScreen() {
     val passwordState = remember {
         mutableStateOf("")
     }
-
+    Log.d("MyLog", "User email: ${auth.currentUser?.email}")  //Проверка с каким Email зашёл юзер
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -43,11 +44,31 @@ fun LoginScreen() {
             passwordState.value = it
         })
         Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = {
+
+        Button(onClick = {                                          //ф-я Входа в аккаунт
+            signIn(auth, emailState.value, passwordState.value)
+        }) {
+            Text(text = "Sign In")
+        }
+
+        Button(onClick = {                                          //ф-я Регистрации аккаунта
             signUp(auth, emailState.value, passwordState.value)
         }) {
             Text(text = "Sign Up")
         }
+
+        Button(onClick = {                                          //ф-я Выход из аккаунта
+            signOut(auth)
+        }) {
+            Text(text = "Sign Out")
+        }
+
+        Button(onClick = {                                          //ф-я Выход из аккаунта
+            deleteAccount(auth, emailState.value, passwordState.value)
+        }) {
+            Text(text = "Delete account")
+        }
+
     }
 }
 
@@ -57,6 +78,34 @@ private fun signUp(auth: FirebaseAuth, email: String, password: String) {
             Log.d("MyLog", "Sign Up successful!")
         } else {
             Log.d("MyLog", "Sign Up failure!")
+        }
+    }
+}
+
+private fun signIn(auth: FirebaseAuth, email: String, password: String) {
+    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+        if (it.isSuccessful) {
+            Log.d("MyLog", "Sign In successful!")
+        } else {
+            Log.d("MyLog", "Sign In failure!")
+        }
+    }
+}
+
+private fun signOut(auth: FirebaseAuth) {
+    auth.signOut()
+    Log.d("MyLog", "Sign Out successful!")
+}
+
+private fun deleteAccount(auth: FirebaseAuth, email: String, password: String) {
+    val credential = EmailAuthProvider.getCredential(email, password)
+    auth.currentUser?.reauthenticate(credential)?.addOnCompleteListener {
+        if (it.isSuccessful) {
+            auth.currentUser?.delete()?.addOnCompleteListener {
+                Log.d("MyLog", "Account deleted successful!")
+            }
+        } else {
+            Log.d("MyLog", "Account deleted failure!")
         }
     }
 }
